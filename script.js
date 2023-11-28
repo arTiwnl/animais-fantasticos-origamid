12f96b3633285afdb7b6c3a8d6a9f0ce993109e4
arTiwnl <contatocarvalhoarthur@gmail.com>
2023-11-27 21:43:34 -0300

    feat: Animação no Scroll

--- script.js
+++ script.js
@@ -83,3 +83,29 @@ function initScrollSuave(){
 
 }
 initScrollSuave();
+
+
+function scrollAnimation(){
+
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if(sections.length) {
+    const halfWindow = window.innerHeight * 0.6;
+
+    function animaScroll() {
+        sections.forEach((section) =>{
+            const sectionTop = section.getBoundingClientRect().top;
+            const isSectionVisible = (sectionTop - halfWindow) < 0;
+            if (isSectionVisible) {
+                section.classList.add('ativo');
+            }else{
+                section.classList.remove('ativo');
+            }
+        });
+    }
+    window.addEventListener('scroll', animaScroll);
+
+}
+
+}
+scrollAnimation();
