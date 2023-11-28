182d2896a96d5021626dfd815f48bb1d40b15573
arTiwnl <contatocarvalhoarthur@gmail.com>
2023-11-27 21:04:43 -0300

    feat: AccordionList

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
 // Navegação por Tabs
+function initTabNav(){
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
 
+    if(tabMenu.length && tabContent.length){
+        tabContent[0].classList.add ('ativo');
 
-if(tabMenu.length && tabContent.length){
-    tabContent[0].classList.add ('ativo');
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+            tabContent[index].classList.add('ativo');
+        }
 
-    function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove('ativo');
+        tabMenu.forEach((itemMenu, index) =>{
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-        tabContent[index].classList.add('ativo');
+
+    }else{
+        console.log('problema nos tabs');
     }
-
-    tabMenu.forEach((itemMenu, index) =>{
-        itemMenu.addEventListener('click', () => {
-            activeTab(index);
-        });
-    });
-
-}else{
-    console.log('problema nos tabs');
 }
+initTabNav();
+
+
+
+// Accordion List
+function initAccordion(){
+    const Accordion = document.querySelectorAll('.js-accordion dt');
+    const activeClass  = 'ativo';
+
+    if(Accordion.length) { 
+        Accordion[0].classList.add(activeClass);
+        Accordion[0].nextElementSibling.classList.add(activeClass);
+
+        function activeAccordion() {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+            
+        }
+
+        Accordion.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
+
+    }else{
+
+        console.log('Accordion com problema');
+    }
+}
+initAccordion();
 
